ed91f0aea0a860b877dbef3d9b72711cbadc03d7
baNanoIT <jealsanchezto@ittepic.edu.mx>
2024-08-28 22:22:24 -0700

    Create ordenamientoArreglo.js

---
+++ ordenamientoArreglo.js
@@ -0,0 +1,10 @@
+//  Ordenamiento de un arreglo: Crea un arreglo de números desordenados y luego ordénalo de menor a 
+//  mayor utilizando algún algoritmo de ordenamiento (p. ej., el método sort()).
+
+    // Arreglo
+    let numerosEnteros = [7,8,9,6,5,4,0,1,2,3]
+    console.log("Arreglo original: " + numerosEnteros);
+
+    // Organizar números
+    numerosEnteros = numerosEnteros.sort()
+    console.log("Arreglo después del método sort(): " + numerosEnteros);
